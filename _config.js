3a28d741f28cc286c7c2b05dda4fe69be666ec10
@iam_Nyange <jnmajanga@gmail.com>
2025-09-20 16:48:47 +0300

    Added more enviroment variables for db user and dbname for more security functionality

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {}
 
 // Use process.env.MONGO_PASSWORD to insert the password securely
 config.mongoURI = {
-    production: `mongodb+srv://johnnyange_db_user:${process.env.MONGO_PASSWORD}@ip1cluster.qkv1jtt.mongodb.net/darkroom?retryWrites=true&w=majority&appName=IP1Cluster`,
-    development: `mongodb+srv://johnnyange_db_user:${process.env.MONGO_PASSWORD}@ip1cluster.qkv1jtt.mongodb.net/darkroom?retryWrites=true&w=majority&appName=IP1Cluster`,
-    test: `mongodb+srv://johnnyange_db_user:${process.env.MONGO_PASSWORD}@ip1cluster.qkv1jtt.mongodb.net/darkroom?retryWrites=true&w=majority&appName=IP1Cluster`,
+    production: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ip1cluster.qkv1jtt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=IP1Cluster`,
+    development: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ip1cluster.qkv1jtt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=IP1Cluster`,
+    test: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ip1cluster.qkv1jtt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=IP1Cluster`,
 }
 
 module.exports = config;
